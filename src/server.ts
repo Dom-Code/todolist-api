204f4d879a2c7d42f8de6d75c2613f4f738409c1
Dom-Code <59179076+Dom-Code@users.noreply.github.com>
2023-09-24 13:55:43 -0500

    methods header added to server.ts

--- src/server.ts
+++ src/server.ts
@@ -48,6 +48,8 @@ router.use(bodyParser.json());
 
 router.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
+
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
 
     if (req.method == 'OPTIONS') {
